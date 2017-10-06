579275539ddd5b7f430b8c8ffb8f1c1f6b611a04
0v1se <dev@0v1se.com>
2017-10-06 15:07:35 +0300

    erc667 receive adapter

---
+++ test/ERC667ReceiveAdapter.js
@@ -0,0 +1,12 @@
+var TokenMock = artifacts.require('./helpers/ERC667Mock.sol');
+var AdapterMock = artifacts.require('./helpers/ERC667ReceiveAdapterMock.sol');
+
+const verifyReceiveAdapter = require('./helpers/verifyReceiveAdapter');
+
+contract('ERC667ReceiveAdapter', function(accounts) {
+  it("should receive tokens", async function() {
+    await verifyReceiveAdapter(accounts[0], TokenMock, AdapterMock, async (token, receiver, value, data) => {
+        await token.transferAndCall(receiver.address, value, data);
+    });
+  });
+});
